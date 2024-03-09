8e08b7262d262c9da77ad68b1ca41ea499564dec
Nick VanCise <nvancisedev@gmail.com>
2024-03-09 14:05:21 -0800

    feat: add note for storybook provided args

--- lib/components/pwa-prompt.stories.tsx
+++ lib/components/pwa-prompt.stories.tsx
@@ -57,6 +57,11 @@ const meta: Meta<PwaPromptPropsAndCustomArgs> = {
       ),
     },
   },
+  argTypes: {
+    useragent: {
+      description: 'Provided by a storybook plugin, this is not an actual prop',
+    },
+  },
   args: {
     useragent:
       'Mozilla/5.0 (iPhone; CPU iPhone OS 17_1_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.1 Mobile/15E148 Safari/604.1',
