2c6c6eaec13252570e2f821e34e9d2d40c0cfab1
Nicholas VanCise <40526638+thenick775@users.noreply.github.com>
2025-02-08 22:14:18 -0800

    chore: upgrade deps (#38)
    * chore: upgrade deps
    
    - upgrades all deps
    
    - fix security vulnerabilities
    
    - upgrade react, permit react 19 in peer deps

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,17 +2,22 @@ import type { StorybookConfig } from '@storybook/react-vite';
 
 const config: StorybookConfig = {
   stories: ['../lib/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     'storybook-addon-useragent',
   ],
+
   framework: {
     name: '@storybook/react-vite',
     options: {},
   },
-  docs: {
-    autodocs: 'tag',
-  },
+
+  docs: {},
+
+  typescript: {
+    reactDocgen: 'react-docgen-typescript'
+  }
 };
 export default config;
